d14ff1c67ee3d4788aba84829318ef117095dd32
github username <github email address>
2018-12-19 21:57:16 -0500

    made an orange header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,20 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 const Header = (props) => {
-  
+
     return(
-      <div>
-        <button
-          className="view-create-button"
-          onClick={props.handleOpenDashboard}>Open Dashboard</button>
-        <button onClick={props.handleViewProducts}>Products</button>
-        <button
-          className="view-create-button"
-          onClick={props.handleCreateOrder}>Create Order</button>
+      <div className="header-container">
+        <div className="header-buttons-container">
+          <button
+            className="view-create-button"
+            onClick={props.handleOpenDashboard}>Open Dashboard</button>
+          <button
+            className="view-create-button"
+            onClick={props.handleViewProducts}>Products</button>
+          <button
+            className="view-create-button"
+            onClick={props.handleCreateOrder}>Create Order</button>
+        </div>
       </div>
     )
   }
